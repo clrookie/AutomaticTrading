a9083d55f3232fb031c3500461cdaa0b4a2d1bee
clrookie <clrookie@naver.com>
2024-01-27 13:50:11 +0900

    aaa

--- test.py
+++ test.py
@@ -1,212 +1,80 @@
+import time
+import pyupbit
+import datetime
+import requests
 
-import pandas as pd
-import FinanceDataReader as fdr
+access = "your-access"
+secret = "your-secret"
+myToken = "xoxb-your-token"
 
-a = 1-1-1-1+1-1+1+1+1+1+1-1-1+1
-print(a)
-# 오늘 날짜 가져오기
+def post_message(token, channel, text):
+    """슬랙 메시지 전송"""
+    response = requests.post("https://slack.com/api/chat.postMessage",
+        headers={"Authorization": "Bearer "+token},
+        data={"channel": channel,"text": text}
+    )
 
+def get_target_price(ticker, k):
+    """변동성 돌파 전략으로 매수 목표가 조회"""
+    df = pyupbit.get_ohlcv(ticker, interval="day", count=2)
+    target_price = df.iloc[0]['close'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * k
+    return target_price
 
-# # 코스피 개장일 목록에서 오늘 날짜가 있는지 확인
-# is_market_open_today = today_date in kospi_market_dates['ListingDate'].dt.date.values
+def get_start_time(ticker):
+    """시작 시간 조회"""
+    df = pyupbit.get_ohlcv(ticker, interval="day", count=1)
+    start_time = df.index[0]
+    return start_time
 
-# # 결과 출력
-# if is_market_open_today:
-#     print("오늘은 코스피의 개장일입니다.")
-# else:
-#     print("오늘은 코스피의 휴장일이거나 개장되지 않은 날입니다.")
+def get_ma15(ticker):
+    """15일 이동 평균선 조회"""
+    df = pyupbit.get_ohlcv(ticker, interval="day", count=15)
+    ma15 = df['close'].rolling(15).mean().iloc[-1]
+    return ma15
 
+def get_balance(ticker):
+    """잔고 조회"""
+    balances = upbit.get_balances()
+    for b in balances:
+        if b['currency'] == ticker:
+            if b['balance'] is not None:
+                return float(b['balance'])
+            else:
+                return 0
+    return 0
 
-# import pandas as pd
-# import yfinance as yf
-# import matplotlib.pyplot as plt
-# from matplotlib import font_manager, rc
+def get_current_price(ticker):
+    """현재가 조회"""
+    return pyupbit.get_orderbook(ticker=ticker)["orderbook_units"][0]["ask_price"]
 
+# 로그인
+upbit = pyupbit.Upbit(access, secret)
+print("autotrade start")
+# 시작 메세지 슬랙 전송
+post_message(myToken,"#crypto", "autotrade start")
 
-# # 주식 데이터 가져오기 (예: 삼성전자, 2022년 1월 1일부터 현재까지)
-# stock_data = yf.download('122630.KS', start='2023-07-01')
+while True:
+    try:
+        now = datetime.datetime.now()
+        start_time = get_start_time("KRW-BTC")
+        end_time = start_time + datetime.timedelta(days=1)
 
-# # 주식 종가 기준으로 5일 평균 계산
-# stock_data['5일평균'] = stock_data['Close'].rolling(window=5).mean()
-
-# # 한글 폰트 설정
-# font_path = "C:/Windows/Fonts/malgun.ttf"  # 사용하는 운영체제의 한글 폰트 경로로 변경
-# font_name = font_manager.FontProperties(fname=font_path).get_name()
-# rc('font', family=font_name)
-
-# # 그래프 그리기
-# plt.figure(figsize=(10, 6))
-# plt.plot(stock_data['Close'], label='종가')
-# plt.plot(stock_data['5일평균'], label='5일 평균선', color='orange')
-# plt.title('주식 가격 및 5일 평균선')
-# plt.xlabel('날짜')
-# plt.ylabel('가격')
-# plt.legend()
-# plt.show()
-
-# symbol_list = { # 대한항공, LG디스플레이,태웅로직스,이월드
-#                 '003490':{'종목명':'대한항공',
-#                 '보유':'False',
-#                 '재매수':'False',
-#                 'profit_rate07_up':'True',
-#                 'profit_rate12_up':'True',
-#                 'profit_rate17_up':'True',
-#                 'profit_rate34_up':'True',
-#                 'profit_rate07_down':'False',
-#                 'profit_rate12_down':'False',
-#                 'profit_rate17_down':'False',
-#                 'profit_rate34_down':'False'},
-
-#                 '034220':{'종목명':'LG디스플레이어',
-#                 '보유':'False',
-#                 '재매수':'False',
-#                 'profit_rate07_up':'True',
-#                 'profit_rate12_up':'True',
-#                 'profit_rate17_up':'True',
-#                 'profit_rate34_up':'True',
-#                 'profit_rate07_down':'False',
-#                 'profit_rate12_down':'False',
-#                 'profit_rate17_down':'False',
-#                 'profit_rate34_down':'False'},
-
-#                 '124560':{'종목명':'태웅로직스',
-#                 '보유':'False',
-#                 '재매수':'False',
-#                 'profit_rate07_up':'True',
-#                 'profit_rate12_up':'True',
-#                 'profit_rate17_up':'True',
-#                 'profit_rate34_up':'True',
-#                 'profit_rate07_down':'False',
-#                 'profit_rate12_down':'False',
-#                 'profit_rate17_down':'False',
-#                 'profit_rate34_down':'False'},
-
-#                 '084680':{'종목명':'이월드',
-#                 '보유':'False',
-#                 '재매수':'False',
-#                 'profit_rate07_up':'True',
-#                 'profit_rate12_up':'True',
-#                 'profit_rate17_up':'True',
-#                 'profit_rate34_up':'True',
-#                 'profit_rate07_down':'False',
-#                 'profit_rate12_down':'False',
-#                 'profit_rate17_down':'False',
-#                 'profit_rate34_down':'False'}               
-#                 }
-# target_buy_count = int(len(symbol_list)) # 매수할 종목 수
-# print(target_buy_count)
-# print("==========")
-
-# for sym in symbol_list:
-#     print(symbol_list[sym]['종목명'])
-
-# symbol_list = {
-# '003490':{'종목명':'대한항공','보유':'0','재매수':'0'},
-# '034220':{'종목명':'LG디스플레이어','보유':'0','재매수':'0'} # 매수종목(대한항공, LG디스플레이,태웅로직스,이월드)
-# }
-
-
-# symbol_list['003490']['재매수'] = True
-
-# chulsoo_neighborhood = symbol_list['003490']['종목명']
-# print(chulsoo_neighborhood)
-
-# chulsoo_neighborhood = symbol_list['034220']['종목명']
-# print(chulsoo_neighborhood)
-
-# chulsoo_neighborhood = symbol_list['003490']['보유']
-# print(chulsoo_neighborhood)
-
-# chulsoo_neighborhood = symbol_list['003490']['재매수']
-# print(chulsoo_neighborhood)
-
-# input1 = 0.1
-# profit_rate07 = 1.007
-# profit_rate12 = 1.012
-# profit_rate17 = 1.017
-# profit_rate34 = 1.034
-
-# profit_rate07_up = True
-# profit_rate12_up = True
-# profit_rate17_up = True
-# profit_rate34_up = True
-
-# profit_rate07_down = False
-# profit_rate12_down = False
-# profit_rate17_down = False
-# profit_rate34_down = False
-
-#  # 거미줄 익절매
-# while 1:
-   
-#     input1 = input("가격 입력: ")
-#     input1 = float(input1)
-
-#     #상향 익절
-#     if input1 > profit_rate34 and profit_rate34_up:
-#         profit_rate34 = 1.034
-#         print(f"==상향 익절 {profit_rate34}")
-#         profit_rate34_up = False
-
-#         profit_rate17_up = False
-#         profit_rate12_up = False
-#         profit_rate07_up = False
-#         profit_rate17_down = True
-#         profit_rate12_down = True
-#         profit_rate07_down = True
-#         continue
-
-#     if input1 > profit_rate17 and profit_rate17_up:
-#         profit_rate17 = 1.017
-#         print(f"==상향 익절 {profit_rate17}")
-#         profit_rate17_up = False
-
-#         profit_rate12_up = False
-#         profit_rate07_up = False
-#         profit_rate12_down = True
-#         profit_rate07_down = True
-#         continue
-
-#     if input1 > profit_rate12 and profit_rate12_up:
-#         profit_rate12 = 1.012
-#         print(f"==상향 익절 {profit_rate12}")
-#         profit_rate12_up = False
-
-#         profit_rate07_up = False
-#         profit_rate07_down = True
-#         continue
-
-#     if input1 > profit_rate07 and profit_rate07_up:
-#         profit_rate07 = 1.007
-#         print(f"==상향 익절 {profit_rate07}")
-#         profit_rate07_up = False
-#         continue
-
-#     # 하향 익절
-#     if input1 <= profit_rate17 and profit_rate17_down:
-#         profit_rate17 = 1.017
-#         print(f"하하하하향 익절 {profit_rate17}")
-#         profit_rate17_down = False
-
-#         profit_rate34_up = True
-#         continue
-
-#     if input1 <= profit_rate12 and profit_rate12_down:
-#         profit_rate12 = 1.012
-#         print(f"하하하하향 익절 {profit_rate12}")
-#         profit_rate12_down = False
-
-#         profit_rate34_up = True
-#         profit_rate17_up = True
-#         continue
-
-#     if input1 <= profit_rate07 and profit_rate07_down:
-#         profit_rate07 = 1.007
-#         print(f"하하하하향 익절 {profit_rate07}")
-#         profit_rate07_down = False
-
-#         profit_rate34_up = True
-#         profit_rate17_up = True
-#         profit_rate12_up = True
-#         continue
-    
+        if start_time < now < end_time - datetime.timedelta(seconds=10):
+            target_price = get_target_price("KRW-BTC", 0.5)
+            ma15 = get_ma15("KRW-BTC")
+            current_price = get_current_price("KRW-BTC")
+            if target_price < current_price and ma15 < current_price:
+                krw = get_balance("KRW")
+                if krw > 5000:
+                    buy_result = upbit.buy_market_order("KRW-BTC", krw*0.9995)
+                    post_message(myToken,"#crypto", "BTC buy : " +str(buy_result))
+        else:
+            btc = get_balance("BTC")
+            if btc > 0.00008:
+                sell_result = upbit.sell_market_order("KRW-BTC", btc*0.9995)
+                post_message(myToken,"#crypto", "BTC buy : " +str(sell_result))
+        time.sleep(1)
+    except Exception as e:
+        print(e)
+        post_message(myToken,"#crypto", e)
+        time.sleep(1)
