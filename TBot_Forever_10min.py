73797f36ba327654141f5b504e4bebf7497cdc66
clrookie <clrookie@naver.com>
2024-03-06 22:30:30 +0900

    ...

--- TBot_Forever_10min.py
+++ TBot_Forever_10min.py
@@ -202,7 +202,7 @@ try:
 
     for sym in symbol_list: # 있으면 일괄 매도
         coin = get_balance(symbol_list[sym]['매도티커'])  # 보유량
-        if coin >= 0.001 : # 최소 거래량
+        if coin > 0 : # 최소 거래량
             sell_result = upbit.sell_market_order(sym, coin)
             if sell_result is not None:
                 send_message(f"[{symbol_list[sym]['종목명']}] {coin} 전량 매도했습니다~")
