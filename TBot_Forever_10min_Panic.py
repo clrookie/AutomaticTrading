7362c27edc12593c766c516fb355a0b9d676ffe2
clrookie <clrookie@naver.com>
2024-03-12 23:39:29 +0900

    ...

--- TBot_Forever_10min_Panic.py
+++ TBot_Forever_10min_Panic.py
@@ -300,8 +300,8 @@ try:
 
             result_rate = ((total_cash+total) / principal * 100) - 100
             formatted_amount = "{:,.0f}원".format(total_cash+total)
-            message_list += f"총 잔고: {formatted_amount}"
-            formatted_amount1 = "{:,.2f}원".format(principal - total_cash+total)
+            message_list += f"총 잔고: {formatted_amount}\n"
+            formatted_amount1 = "{:,0f}원".format((total_cash+total)-principal)
             formatted_amount2 = "{:,.2f}%".format(result_rate)
             message_list += f"수익율: {formatted_amount1} ({formatted_amount2})"
 
