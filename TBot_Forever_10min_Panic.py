c6bc6c8f3b2a8d698706b006d4f1ef1c9c7188c7
clrookie <clrookie@naver.com>
2024-03-16 08:41:51 +0900

    ...

--- TBot_Forever_10min_Panic.py
+++ TBot_Forever_10min_Panic.py
@@ -50,7 +50,7 @@ try:
     panic_volume_rate_max = 3 #2배
     panic_volume_rate_max_more = 4.5 #3배
 
-    greed_volume_rate = 2 #2배
+    greed_volume_rate = 2 #1배
     greed_volume_rate_max = 4 #3배
     greed_volume_rate_max_more = 6 #4배
     
