dc9fc74fd4a5380bf7bfdc07d13d81d50528222e
clrookie <clrookie@naver.com>
2024-03-29 08:44:18 +0900

    ...

--- TBot_Forever_1min_Panic.py
+++ TBot_Forever_1min_Panic.py
@@ -132,7 +132,7 @@ try:
 
         if df.index[0].minute != last_min:    # 10분 캔들 갱신
 
-            time.sleep(4) # 데이터 갱신 보정
+            time.sleep(5) # 데이터 갱신 보정
 
             last_min = df.index[0].minute
 
