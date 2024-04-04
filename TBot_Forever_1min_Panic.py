6d6257cbb2130e7cf1c8edab28deb85a88e339bb
clrookie <clrookie@naver.com>
2024-04-05 00:43:29 +0900

    ...

--- TBot_Forever_1min_Panic.py
+++ TBot_Forever_1min_Panic.py
@@ -279,7 +279,7 @@ try:
                         # 양봉이니?
                         if last_open <= last_close:
 
-                            if symbol_list[sym]['탐욕에너지'] < 2:
+                            if symbol_list[sym]['탐욕에너지'] < 1:
                                 symbol_list[sym]['탐욕에너지'] += 1
 
                             if symbol_list[sym]['공포적립'] > symbol_list[sym]['탐욕에너지']:
