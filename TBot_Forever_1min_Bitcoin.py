67f501321691586157129abc22120f10d29b2777
clrookie <clrookie@naver.com>
2024-04-14 11:36:38 +0900

    ..

--- TBot_Forever_1min_Bitcoin.py
+++ TBot_Forever_1min_Bitcoin.py
@@ -222,7 +222,7 @@ try:
                             r_last_volume = round((current_price*last_volume)/buy_rate)
 
                             if symbol_list[sym]['잔여예산'] > 0:
-                                rate = (1 - (symbol_list[sym]['잔여예산'] / allotment_budget)) * 7
+                                rate = (1 - (symbol_list[sym]['잔여예산'] / allotment_budget)) * 5
                                 rate = round(rate,2)
                                 if rate < 1: rate = 1
 
